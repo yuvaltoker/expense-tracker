a4fdffa8e309a28411dcd4aeedb01c2db3a421ec
yuval toker <yuvalt2710@gmail.com>
2024-01-08 17:24:14 +0200

    used side bar example as infrastructure code

---
+++ app.js
@@ -0,0 +1,32 @@
+// const body = document.body
+// const title_div = document.createElement('div')
+// title_div.textContent = 'Expense Tracker'
+// title_div.className = 'main_title'
+
+// body.append(title_div)
+
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
