cc127fdc912a5aa395d6b056209677ea76989366
yuval toker <yuvalt2710@gmail.com>
2024-01-10 04:34:05 +0200

    managed to switch between pages (divs)

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ function createSidebarHeader(text) {
 function createMenuListItem(name) {
     const list_item = document.createElement('li')
     list_item.textContent = name
+    list_item.id = name.toLowerCase() + 'Item'
+    // Adding event listeners using JavaScript
+    list_item.addEventListener('click', function() {
+        handlePageItemClick(name);
+    });
     return list_item
 }
 
-function createMenuList() {
+function createMenuList(pages_names) {
     const menu_list = document.createElement('ul')
-    const items = ['Item 1', 'Item 2', 'Item 3']
-    const list_items = items.map((item) => createMenuListItem(item))
+    const list_items = pages_names.map((page_name) => createMenuListItem(page_name))
     list_items.forEach(list_item => {
         list_item.className = 'sidebar-list-item'
         appendComponent(menu_list, list_item)
@@ -27,31 +31,59 @@ function createMenuList() {
     return menu_list
 }
 
-function createSidebar() {
+function createSidebar(pages_names) {
     // main of sidebar
     const side_bar = document.createElement('nav')
     side_bar.className = 'sidebar'
     const side_bar_header = createSidebarHeader('E.T')
     appendComponent(side_bar, side_bar_header)
-    const side_bar_menu = createMenuList()
+    const side_bar_menu = createMenuList(pages_names)
     appendComponent(side_bar, side_bar_menu)
     return side_bar
 }
 
-function createPage(name) {
+function createPage(page_name) {
     const page = document.createElement('div')
-    page.textContent = name
+    page.textContent = page_name
+    page.id = page_name.toLowerCase() + 'Page'
     return page
 }
 
-function createContent() {
-    const content = document.createElement('div')
-    const pages_names = ['Page 1', 'Page 2', 'Page 3']
-    const list_pages = pages_names.map((page_name) => createPage(page_name))
+function hideAllPages() {
+    const pages = document.getElementById('pages')
+    for (const child of pages.children) {
+        child.hidden = true
+    }
+}
+
+function showPage(page_name) {
+    const page = document.getElementById(page_name.toLowerCase() + 'Page')
+    page.hidden = false
+}
+
+function handlePageItemClick(page_name) {
+    hideAllPages()
+    showPage(page_name)
+}
+
+function createPages(page_names) {
+    const pages = document.createElement('div')
+    pages.id = 'pages'
+    pages.className = 'pages'
+    const list_pages = page_names.map((page_name) => createPage(page_name))
     list_pages.forEach(page => {
         page.className = 'page'
-        appendComponent(content, page)
+        appendComponent(pages, page)
     })
+    return pages
+}
+
+function createContent(page_names) {
+    const content = document.createElement('div')
+    content.id = 'content'
+    content.className = 'content'
+    const pages = createPages(page_names)
+    appendComponent(content, pages)
     return content
 }
 
@@ -61,17 +93,22 @@ function createContainer() {
     return container
 }
 
-function createComponents() {
+function createComponents(page_names) {
     const container = createContainer()
-    const side_bar = createSidebar()
+    const side_bar = createSidebar(page_names)
     appendComponent(container, side_bar)
-    const content = createContent()
+    const content = createContent(page_names)
     appendComponent(container, content)
     return container
 }
 
-const container = createComponents()
+// pageName must be unique
+const page_names = ['Home', 'About', 'Contact'];
+
+const container = createComponents(page_names)
 body.append(container)
+// hideAllPages()
+// showPage(page_names[1])
 
 const sidebar = document.querySelector('.sidebar')
 const content = document.querySelector('.content')
@@ -82,5 +119,3 @@ sidebar.addEventListener('mouseleave', () => container.classList.remove('hover-s
 
 content.addEventListener('mouseenter', () => container.classList.add('hover-content'))
 content.addEventListener('mouseleave', () => container.classList.remove('hover-content'))
-
-
